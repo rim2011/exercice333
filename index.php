ed59eab7b4e7077e72124879b9567de24a86682b
rim2011 <43763622+rim2011@users.noreply.github.com>
2018-11-05 21:20:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php $var="3.52 kilomètres";
+$var2 = (double) $var;
+echo "\$var2= ",$var2,"<br />";
+$var3 = (integer) $var2;
+echo "\$var3= ",$var3,"<br />";
+$var4 = (boolean) $var3;
+echo "\$var4= ",$var4,"<br />";
+?>
